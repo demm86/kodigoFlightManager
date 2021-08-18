1b4cf2edc88126949afc47d8abdbe63ef27b65cb
ISMAEL EDUARDO LATIN BRENES <ismael.latin18@itca.edu.sv>
2021-08-18 10:42:36 -0600

    Creación de menú principal

--- src/main/java/projectSolid/Flight.java
+++ src/main/java/projectSolid/Flight.java
@@ -1,18 +1,31 @@
 package projectSolid;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import java.time.LocalDateTime;
 
 @Data
 public class Flight {
+    @Getter @Setter
     private int id;
+    @Getter @Setter
     private String code;
+    @Getter @Setter
     private Aircraft aircraft;
+    @Getter @Setter
     private Airport departureAirport;
+    @Getter @Setter
     private Airport arrivalAirport;
+    @Getter @Setter
     private LocalDateTime departureTime;
+    @Getter @Setter
     private LocalDateTime arrivalTime;
+    @Getter @Setter
     private LocalDateTime delayDepartureTime;
+    @Getter @Setter
     private LocalDateTime delayArrivalTime;
+    @Getter @Setter
     private Airport delayArrivalAirport;
+    @Getter @Setter
     private FlightStatus flightStatus;
 }
