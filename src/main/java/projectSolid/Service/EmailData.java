b1e81e271ba668be8b1087183d33acc11c6be358
@Martinez182 <emre21011992@gmail.com>
2021-08-19 14:42:10 -0600

    Creacion del metodo send email

---
+++ src/main/java/projectSolid/Service/EmailData.java
@@ -0,0 +1,21 @@
+package projectSolid.Service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class EmailData {
+
+        @Getter
+        @Setter
+        private String sender;
+        @Getter @Setter
+        private String password;
+        @Getter @Setter
+        private String receiver;
+
+        public EmailData(){
+            this.sender ="emre21011992@gmail.com";
+            this.password = "Carlos@92";
+            this.receiver = "emre21011992@gmail.com";
+        }
+}
