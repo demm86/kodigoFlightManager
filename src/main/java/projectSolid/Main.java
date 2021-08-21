340da57a7ed5e0ad83a8d72dfd5656623b1dc9c2
@Martinez182 <emre21011992@gmail.com>
2021-08-20 21:51:19 -0600

    aircraft list

--- src/main/java/projectSolid/Main.java
+++ src/main/java/projectSolid/Main.java
@@ -1,12 +1,19 @@
 package projectSolid;
 
+import projectSolid.Entities.AircraftType;
+import projectSolid.Implementation.AircraftTypesServices;
 import projectSolid.Service.Menu;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
     public static Menu menu = new Menu();
 
     public static void main(String[] args){
+
         menu.shoMenu();
+
     }
 }
