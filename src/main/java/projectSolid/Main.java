4943f5818b40ca992e88cd555a60454b039595b2
Stanley <wltrstnly@hotmail.com>
2021-08-20 21:11:59 -0600

    Development

--- src/main/java/projectSolid/Main.java
+++ src/main/java/projectSolid/Main.java
@@ -2,11 +2,11 @@ package projectSolid;
 
 import projectSolid.Service.Menu;
 
-
 public class Main {
     public static Menu menu = new Menu();
 
     public static void main(String[] args){
+
         menu.shoMenu();
     }
 }
